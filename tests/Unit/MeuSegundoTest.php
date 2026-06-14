113351dd48766b990becb6bcf7479ed8b014f77c
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-06-14 12:45:42 -0300

    executando testes de forma criteriosa -comentarios

--- tests/Unit/MeuSegundoTest.php
+++ tests/Unit/MeuSegundoTest.php
@@ -5,3 +5,7 @@ test('O meu segundo teste', function () {
     $name = "Matheus";
     expect($name)->toBeString();
 });
+
+// php artisan test --suitetest=Unit --filter=MeuSegundoTest
+// php artisan test --suitetest=Feature --filter=MeuPrimeiroTest
+// php artisan test
